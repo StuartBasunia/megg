87408dcb240b7ec4424709e218812121358d11f2
StuartBasunia <68786421+StuartBasunia@users.noreply.github.com>
2022-04-08 19:12:40 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -43,8 +43,7 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-            f'<b>Bot Uptime:</b> {currentTime}\n'\
+    stats = f'<b>Bot Uptime:</b> {currentTime}\n'\
             f'<b>OS Uptime:</b> {osUptime}\n\n'\
             f'<b>Total Disk Space:</b> {total}\n'\
             f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\

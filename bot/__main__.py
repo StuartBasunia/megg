49f57eb51e1828c58eb59299f756c3dd7b911e01
StuartBasunia <68786421+StuartBasunia@users.noreply.github.com>
2022-04-08 19:23:43 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -57,7 +57,7 @@ def stats(update, context):
             f'<b>Memory Total:</b> {mem_t}\n'\
             f'<b>Memory Free:</b> {mem_a}\n'\
             f'<b>Memory Used:</b> {mem_u}\n'
-    sendMessage(stats, context.bot, update.message)
+    sendMessage(stats, context.bot, update)
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
